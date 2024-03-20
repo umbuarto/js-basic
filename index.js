6a91bcca127133e11d8480cdde1ba40638ab75b5
Arto Ngguli Hunga <artorimex@gmail.com>
2024-03-20 20:50:37 +0800

    fdfg

---
+++ index.js
@@ -0,0 +1,43 @@
+var umur = 17;
+
+if (umur >= 17) {
+  consule.log("cukup umur untuk bikin ktp");
+} else {
+  consule.log("Belum Cukup Umur");
+}
+
+var minuman = "kopi";
+
+switch (kopi) {
+  case "kopi":
+    consule.log("yes");
+    break;
+
+  case "teh":
+    consule.log("no");
+    break;
+
+  default:
+    break;
+}
+
+var anak = 17;
+var tinggi = 150;
+
+switch (anak) {
+  case 17:
+    switch (tinggi) {
+      case 150:
+        consule.log("Boleh Masuk");
+        break;
+
+      default:
+        console.log("Tidak bisa");
+        break;
+    }
+    break;
+
+  default:
+    console.log("Tidak bisa");
+    break;
+}
